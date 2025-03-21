cf00c2b5a63345cea6dc61b0eb9863cb1fdbea8f
ChoHyunWoo <gusdndlek12@naver.com>
2025-03-21 14:38:26 +0900

    saveOnDisk & save test case

--- src/main/kotlin/domain/wiseSaying/entity/WiseSaying.kt
+++ src/main/kotlin/domain/wiseSaying/entity/WiseSaying.kt
@@ -1,11 +1,21 @@
 package com.think.domain.wiseSaying.entity
 
 data class WiseSaying(
-    val id: Int =0,
+    val id: Int = 0,
     val saying: String,
     val author: String
 ) {
     fun isNew(): Boolean {
         return id == 0
     }
+
+    val jsonStr: String
+        get() = """
+            {
+                "id": $id,
+                "saying": "$saying",
+                "author": "$author"
+            }
+        """.trimIndent()
+
 }
